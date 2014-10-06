949074517472b60f848abea744b279d7f716d664
ian <ian.bowman@mac.com>
2014-10-06 15:51:06 +0100

    Initial Commit

---
+++ Where2Meet/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Where2Meet
+//
+//  Created by Ray Badger on 06/10/2014.
+//  Copyright (c) 2014 TKA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
