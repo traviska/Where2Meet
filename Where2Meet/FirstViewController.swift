e238f204f1000592b0427aa315d6f771f17ba271
ian <ian.bowman@mac.com>
2014-10-07 11:29:46 +0100

    Added map view, added ability to create a list of postcodes. Need to link the two together.

--- Where2Meet/FirstViewController.swift
+++ Where2Meet/FirstViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    let appDele = UIApplication.sharedApplication().delegate as AppDelegate
+    
+    @IBOutlet var postcodeField: UITextField!
+    @IBOutlet var locationList: UITableView!
+    
+    @IBOutlet var addButton: UIButton!
+    
+    @IBAction func addLocation(sender: UIButton) {
+        if (!postcodeField.text.isEmpty) {
+            appDele.postcodes.append(postcodeField.text)
+            self.locationList.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +32,19 @@ class FirstViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return appDele.postcodes.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell:UITableViewCell = self.locationList.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel?.text = appDele.postcodes[indexPath.row]
+        return cell
     }
 
 
--- Where2Meet/SecondViewController.swift
+++ Where2Meet/SecondViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 1
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
